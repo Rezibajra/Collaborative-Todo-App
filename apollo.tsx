7d023f80564419847794dfd3367dcf4c11fa19e2
rezibajracharya <rezi.bajracharya@bajratechnologies.com>
2024-12-08 20:25:52 +0545

    Header Authentication

--- apollo.tsx
+++ apollo.tsx
@@ -1,8 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 const URI = 'https://637b-2404-7c00-41-e0cb-9fb6-e0e1-99e7-8c49.ngrok-free.app'
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
     uri: URI,
+})
+
+const authLink = setContext(async (_, { headers }) => {
+    const token = await AsyncStorage.getItem('token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token || '',
+        }
+    }
+});
+
+export const client = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 });
--- app/ProjectsScreen.tsx
+++ app/ProjectsScreen.tsx
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
-import { StyleSheet, FlatList, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, FlatList, TextInput, KeyboardAvoidingView, Platform, Alert } from 'react-native';
 import ProjectItem from '../components/ProjectItem';
 import { Text, View } from '@/components/Themed';
 import { useTheme } from '@react-navigation/native';
+import { useQuery , gql} from '@apollo/client';
+
+const MY_PROJECTS = gql `
+query myTaskLists {
+  myTaskLists {
+    id
+    title
+    createdAt
+  }
+}
+`;
 
 export default function ProjectsScreen() {
-  const [project, setProjects] = useState([{
-    id: '1',
-    title: 'Project 1',
-    createdAt: '2d'
-  }, {
-    id: '2',
-    title: 'Project 2',
-    createdAt: '2d'
-  }, {
-    id: '3',
-    title: 'Project 3',
-    createdAt: '2d'
-  }]);
+  const [project, setProjects] = useState([]);
+
+  const { data, error, loading } = useQuery(MY_PROJECTS)
+
+  useEffect(() => {
+    if (error) {
+      Alert.alert('Error fetching projects', error.message);
+    }
+  }, [error])
+
+  useEffect(() => {
+    if (data) {
+      setProjects(data.myTaskLists);
+    }
+  }, [data])
+
   return (
       <View style={styles.container}>
         <FlatList
